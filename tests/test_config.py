7d4f5e0e6b1a6fb36c94312003c386d37e6cefe3
dennisdnyce <jumaspay@gmail.com>
2019-01-09 01:53:23 +0300

    [start #163073601] Write failing config tests

---
+++ tests/test_config.py
@@ -0,0 +1,45 @@
+import pytest
+import unittest
+from app import create_app
+from flask import Flask
+import instance
+
+
+class TestConfig(unittest.TestCase):
+    ''' tests the main configuration class '''
+    def test_config(self):
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(instance.config.Config)
+        self.assertTrue(app.config['DEBUG'] is False)
+        self.assertTrue(app.config['CSRF_ENABLED'] is True)
+        self.assertTrue(app.config['SECRET_KEY'] is 'thisismysecretkeywhydontyouuseyours')
+
+    def test_development(self):
+        ''' tests the configuration for the development environment '''
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(instance.config.DevelopmentConfig)
+        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertTrue(app.config['TESTING'] is True)
+
+    def test_staging(self):
+        ''' tests the configuration for the staging environment '''
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(instance.config.StagingConfig)
+        self.assertTrue(app.config['DEBUG'] is True)
+
+    def test_testing(self):
+        ''' tests the configuration for the testing environment '''
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(instance.config.TestingConfig)
+        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertTrue(app.config['TESTING'] is True)
+
+    def test_production(self):
+        ''' tests the configuration for the production environment '''
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(instance.config.ProductionConfig)
+        self.assertTrue(app.config['DEBUG'] is False)
+        self.assertTrue(app.config['TESTING'] is False)
+
+if __name__ == '__main__':
+    unittest.main()
