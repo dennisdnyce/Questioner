4d4dc2ea0af8e940ea5267bdec639e154615c6b2
dennisdnyce <jumaspay@gmail.com>
2019-01-11 00:24:59 +0300

    [finishes #163044923] Reserve Meetup

--- app/api/v1/views/meetup_views.py
+++ app/api/v1/views/meetup_views.py
@@ -33,7 +33,7 @@ def post_meetup():
 
 @mymeets.route('/meetups/<int:meetupId>', methods=['GET'])
 def get_meetup(meetupId):
-    ''' method to get single posted meetup '''
+    ''' method to fetch single posted meetup '''
     i = meeting.get_a_meetup(meetupId)
     if i:
         data = request.get_json()
