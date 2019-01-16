986b794b4621f025dbbcbefc824bdde8a992a5c7
dennisdnyce <jumaspay@gmail.com>
2019-01-16 23:11:38 +0300

    [finishes #163269663] Add Test Db on Travis-CI

--- manage.py
+++ manage.py
@@ -1,4 +1,8 @@
 from app.api.v2.models.database import QuestionerDatabaseConnection
+from app.api.v2.models.database_test import QuestionerTestDatabase
 
 connector = QuestionerDatabaseConnection()
+TestConnector = QuestionerTestDatabase()
+
 connector.create_questioner_tables()
+TestConnector.create_questioner_test_tables()
